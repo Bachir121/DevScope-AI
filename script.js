63f4f26c4801b05d93ad01aab3843fa254149eaf
Bachir121 <102300972+Bachir121@users.noreply.github.com>
2025-06-28 02:41:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 document.getElementById('ai-form').addEventListener('submit', async (e) => {
   e.preventDefault();
   const idea = document.getElementById('projectIdea').value;
-  const res = await fetch('/api/generate', {
+  const res = await fetch('https://devscope-ai.onrender.com/api/generate', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ idea })
